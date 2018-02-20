ec8f4887211fd90698f6fe2dc49216b1a58852bb
Jacob Ollila <jacob@gouconnect.com>
2018-02-20 09:10:23 -0500

    Added comment

--- app/src/main/java/edu/wit/mobileapp/hiddengem/LocationSelectionActivity.java
+++ app/src/main/java/edu/wit/mobileapp/hiddengem/LocationSelectionActivity.java
@@ -2,6 +2,7 @@ package edu.wit.mobileapp.hiddengem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LocationSelectionActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class LocationSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_selection);
+
+        Log.v("Hidden Gem", "Test to see if I can push to GitHub");
     }
 }
