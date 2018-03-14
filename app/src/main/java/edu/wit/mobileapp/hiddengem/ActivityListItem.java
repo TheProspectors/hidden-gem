6b1d54d46ba076aa39ca01ae5eeeee3762ccd903
Ford Polia <poliaf@wit.edu>
2018-03-14 15:06:19 -0400

    Added screen to select activity

---
+++ app/src/main/java/edu/wit/mobileapp/hiddengem/ActivityListItem.java
@@ -0,0 +1,12 @@
+package edu.wit.mobileapp.hiddengem;
+
+import android.graphics.drawable.VectorDrawable;
+
+/**
+ * Created by poliaf on 3/7/18.
+ */
+
+public class ActivityListItem {
+    public VectorDrawable image;
+    public String label;
+}
