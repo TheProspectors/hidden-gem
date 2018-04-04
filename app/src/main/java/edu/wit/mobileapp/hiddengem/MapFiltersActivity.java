914bd45dac15d233178c115cd06c3f9c433caba6
Jake Mathews <mathewsj2@wit.edu>
2018-04-04 11:14:51 -0400

    Formatting and added missing dependency

--- app/src/main/java/edu/wit/mobileapp/hiddengem/MapFiltersActivity.java
+++ app/src/main/java/edu/wit/mobileapp/hiddengem/MapFiltersActivity.java
@@ -17,14 +17,42 @@ import android.widget.SeekBar;
  * Created by ollilaj on 3/25/2018.
  */
 
-public class MapFiltersActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MapFiltersActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    // Sets the increments for each seekBar
+    private SeekBar.OnSeekBarChangeListener seekBarListener = new SeekBar.OnSeekBarChangeListener() {
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            if (seekBar.getId() == R.id.price_seekbar || seekBar.getId() == R.id.ratings_seekbar) {
+                final int inBetween = progress % 25;
+
+                if (inBetween > 12.5) {
+                    seekBar.setProgress(progress - inBetween + 25);
+                } else {
+                    seekBar.setProgress(progress - inBetween);
+                }
+            } else if (seekBar.getId() == R.id.distance_seekbar) {
+                int inBetween = progress % 5;
+
+                if (inBetween > 2.5) {
+                    seekBar.setProgress(progress - inBetween + 5);
+                } else {
+                    seekBar.setProgress(progress - inBetween);
+                }
+            }
+        }
+
+        public void onStartTrackingTouch(SeekBar seekBar) {
+        }
+
+        public void onStopTrackingTouch(SeekBar seekBar) {
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_filters);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         final SeekBar priceSeekBar = (SeekBar) findViewById(R.id.price_seekbar);
@@ -40,12 +68,12 @@ public class MapFiltersActivity extends AppCompatActivity implements NavigationV
         setSeekBarClickListener(distanceSeekBar);
         setSeekBarClickListener(ratingsSeekBar);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close){
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
 
             @Override
             public void onDrawerClosed(View drawerView) {
@@ -55,13 +83,13 @@ public class MapFiltersActivity extends AppCompatActivity implements NavigationV
                 int distanceValue = distanceSeekBar.getProgress();
                 int ratingsValue = ratingsSeekBar.getProgress();
 
-                if(priceValue != 0) {
+                if (priceValue != 0) {
                     priceValue = (priceValue / 25) + 1;
                 }
-                if(distanceValue != 0) {
+                if (distanceValue != 0) {
                     distanceValue = (distanceValue / 5) + 1;
                 }
-                if(ratingsValue != 0) {
+                if (ratingsValue != 0) {
                     ratingsValue = (ratingsValue / 25) + 1;
                 }
 
@@ -74,12 +102,10 @@ public class MapFiltersActivity extends AppCompatActivity implements NavigationV
 
     // Prevents the click event on the navigation view from firing if the click was performed on the seekBar
     private void setSeekBarClickListener(SeekBar seekBar) {
-
         seekBar.setOnTouchListener(new NavigationView.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-
-                int action = event.getAction();
+                final int action = event.getAction();
                 switch (action) {
                     case MotionEvent.ACTION_DOWN:
                         // Disallow Drawer to intercept touch events.
@@ -100,46 +126,9 @@ public class MapFiltersActivity extends AppCompatActivity implements NavigationV
 
     }
 
-    // Sets the increments for each seekBar
-    private SeekBar.OnSeekBarChangeListener seekBarListener = new SeekBar.OnSeekBarChangeListener() {
-
-        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-            if(seekBar.getId() == R.id.price_seekbar || seekBar.getId() == R.id.ratings_seekbar) {
-
-                int inBetween = progress % 25;
-
-                if(inBetween > 12.5) {
-                    seekBar.setProgress(progress - inBetween + 25);
-                }
-                else {
-                    seekBar.setProgress(progress - inBetween);
-                }
-
-            }
-            else if(seekBar.getId() == R.id.distance_seekbar) {
-
-                int inBetween = progress % 5;
-
-                if(inBetween > 2.5) {
-                    seekBar.setProgress(progress - inBetween + 5);
-                }
-                else {
-                    seekBar.setProgress(progress - inBetween);
-                }
-
-            }
-
-        }
-
-        public void onStartTrackingTouch(SeekBar seekBar) {}
-
-        public void onStopTrackingTouch(SeekBar seekBar) {}
-    };
-
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
